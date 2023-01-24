08245b78b3a39aed798ac91501b9d80fa791222d
Nazir Hussain <56648155+nazir-hussain@users.noreply.github.com>
2023-01-25 00:09:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+const starEl = document.querySelectorAll(".fa-star");
+const emojisEl = document.querySelectorAll(".far")
+const colorsArray = ["red", "orange", "lightblue", "lightgreen", "green"]
+
+updateRating(0);
+
+starEl.forEach((starEl, index)=>{
+    starEl.addEventListener("click", () => {
+        updateRating(index)
+    })
+})
+
+function updateRating(index) {
+    starEl.forEach((starEl, idx) => {
+        if (idx < index + 1) {
+            starEl.classList.add("active");
+        }else{
+            starEl.classList.remove("active")
+        }
+    })
+
+    emojisEl.forEach(emojisEl => {
+        emojisEl.getElementsByClassName.transform = `translateX(-${index * 50}px)`;
+        emojisEl.style.color= colorsArray[index]
+    })
+    
+}
